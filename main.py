ae3a04ed35267ca5ed3da8e910c60022445f66c7
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2022-07-30 01:19:09 +0000

    Added comments

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-PLACEHOLDER = "name"
+PLACEHOLDER = "name" #appears on original letter template
 
+#list of names
 with open ("mail-merge-project-start/Input/Names/invited_names.txt") as names_file:
-  names = names_file.readlines()
+  names = names_file.readlines() 
 
-with open ("mail-merge-project-start/Input/Letters/starting_letter.txt") as letter_file:
+with open ("mail-merge-project-start/Input/Letters/starting_letter.txt") as letter_file: #letter template
   letter_contents = letter_file.read()
   for name in names:
     stripped_name = name.strip()
-    new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+    new_letter = letter_contents.replace(PLACEHOLDER, stripped_name) #replaces the "name" placeholder with the person's name
     with open (f"mail-merge-project-start/Input/Output/ReadyToSend/{stripped_name}.txt", "w") as completed_letter:
-      completed_letter.write(new_letter)
+      completed_letter.write(new_letter) #creates a new letter for each person
